b85600c3a0e6474d898e4fe71d7e6bfa698bd12d
Alan <alanjhyu@gmail.com>
2021-02-26 01:53:48 -0600

    Update better-quercus.js

--- better-quercus.js
+++ better-quercus.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Better Quercus
-// @namespace    https://github.com/alanjyu/better-quercus
-// @version      0.2
+// @namespace    https://alanjyu.com
+// @homepageURL  https://github.com/alanjyu/better-quercus
+// @version      0.1
 // @description  Better Quercus; still experimental.
 // @author       Alan J. Yu
 // @match        http*://q.utoronto.ca/courses/*/files/*
@@ -11,13 +12,9 @@
 (function() {
     'use strict';
 
-    var body = document.body
-    var doc_preview = document.querySelector('#doc_preview');
-    var footer = document.querySelector('.module-sequence-footer-content');
-    var menu = document.querySelector('#left-side');
+    var docPreview = document.querySelector('#doc_preview');
     var h2 = document.querySelector('#content > h2');
 
-    body.classList.remove('course-menu-expanded');
-    menu.style.display = 'none';
+    document.body.classList.remove('course-menu-expanded');
     h2.style.display = 'none';
 })();
