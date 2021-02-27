973ec130e3855e6707d8baec2e4f38efbba1f642
Alan <alanjhyu@gmail.com>
2021-02-26 19:33:43 -0600

    Optimization

--- better-quercus.user.js
+++ better-quercus.user.js
@@ -5,7 +5,7 @@
 // @version      1.1
 // @description  Providing a better Quercus experience
 // @author       Alan J. Yu
-// @include        http*://q.utoronto.ca/courses/*/files/*
+// @match        http*://q.utoronto.ca/courses/*/files/*
 // @updateURL    https://raw.githubusercontent.com/alanjyu/better-quercus/main/better-quercus.user.js
 // @downloadURL  https://raw.githubusercontent.com/alanjyu/better-quercus/main/better-quercus.user.js
 // @supportURL   https://github.com/alanjyu/better-quercus.js/issues
@@ -15,13 +15,20 @@
     'use strict';
 
     const docPreview = document.getElementById('doc_preview');
-    const h2 = document.querySelector('#content > h2');
     const downloadText = document.getElementById('content').getElementsByTagName('div')[0];
     const footer = document.getElementById('sequence_footer');
 
     // maximize doc preview
+    var styleSheet = document.createElement('style')
+    styleSheet.innerHTML =
+        '#sequence_footer{position: absolute; top: -54px; right: 25px; padding: 0;} \
+         #breadcrumbs{overflow: hidden; max-width: 50%;} \
+         #dov_preview > div{overflow-x: hidden;} \
+         .module-sequence-footer{height: 38px;} \
+         .module-sequence-footer-content{border: none !important; animation: none !important; background: none !important; padding: 0!important;}';
+    document.body.appendChild(styleSheet);
     document.body.classList.remove('course-menu-expanded');
-    h2.style.display = 'none';
+    document.getElementById('content').getElementsByTagName('h2')[0].style.display = 'none';
 
     // turn download text into a button
     downloadText.classList.add('Button');
@@ -29,18 +36,8 @@
     downloadText.querySelector('span > a').innerHTML = 'Download';
     downloadText.querySelector('span').style.cssText = 'font-size: 1rem;';
 
-    // wrap header text at 50% width
-    document.getElementById('breadcrumbs').style.cssText = 'overflow: hidden; max-width: 50%; text-overflow: ellipsis;';
-
-    // move footer to top
-    footer.style.cssText = 'position: absolute; top: -54px; right: 25px; padding: 0;';
-
     // for delayed script events
     window.addEventListener('load', function(){
-        // reshape footer
-        footer.querySelector('.module-sequence-footer').style.cssText = 'height: 38px;'
-        footer.querySelector('.module-sequence-footer > .module-sequence-footer-content').style.cssText = 'border: none; background: none; animation: none; padding: 0;';
-
         // maximize doc preview
         docPreview.getElementsByTagName('div')[0].style.overflow = 'hidden';
         docPreview.getElementsByTagName('div')[0].getElementsByTagName('iframe')[0].style.minHeight = '0';
