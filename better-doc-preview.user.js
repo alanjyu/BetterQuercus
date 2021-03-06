2b73fb4aa8cae7dc7c9d9af0a25faaeee798b1de
Alan J. Yu <alanjhyu@gmail.com>
2021-03-06 15:56:03 -0600

    Implement responsive

--- better-doc-preview.user.js
+++ better-doc-preview.user.js
@@ -2,8 +2,9 @@
 // @name         Better Document Preview
 // @namespace    https://alanjyu.com
 // @homepageURL  https://github.com/alanjyu/better-quercus.js
-// @version      1.2
+// @version      1.3.1
 // @description  Maximizes the document preview window
+// @run-at       document-idle
 // @author       Alan J. Yu
 // @match        http*://q.utoronto.ca/courses/*/files/*
 // @updateURL    https://raw.githubusercontent.com/alanjyu/better-quercus/main/better-doc-preview.user.js
@@ -14,32 +15,48 @@
 (function() {
     'use strict';
 
-    const docPreview = document.getElementById('doc_preview');
-    const downloadText = document.getElementById('content').getElementsByTagName('div')[0];
-    const footer = document.getElementById('sequence_footer');
-
     // maximize doc preview
     var styleSheet = document.createElement('style')
     styleSheet.innerHTML =
-        '#sequence_footer{position: absolute; top: -54px; right: 25px; padding: 0;} \
+        '#content{padding: 4px 12px;} \
+         #sequence_footer{position: absolute; top: -54px; right: 25px; padding: 0;} \
          #breadcrumbs{overflow: hidden; max-width: 50%;} \
-         #dov_preview > div{overflow-x: hidden;} \
          .module-sequence-footer{height: 38px;} \
-         .module-sequence-footer-content{border: none !important; animation: none !important; background: none !important; padding: 0!important;}';
+         .module-sequence-footer-content{border: none!important; animation: none!important; background: none!important; padding: 0!important;}';
     document.body.appendChild(styleSheet);
-    document.body.classList.remove('course-menu-expanded');
     document.getElementById('content').getElementsByTagName('h2')[0].style.display = 'none';
 
+    // responsive sidebar
+    function responsiveSidebar() {
+        if (window.innerWidth < 1150 && document.body.classList.contains('course-menu-expanded')) {
+            document.body.classList.remove('course-menu-expanded');
+        } else if (window.innerWidth >= 1150 && !document.body.classList.contains('course-menu-expanded')){
+            document.body.classList.add('course-menu-expanded');
+        }
+    }
+    responsiveSidebar();
+    window.addEventListener('resize', evt => {
+        responsiveSidebar();
+    });
+
     // turn download text into a button
+    var downloadText = document.getElementById('content').getElementsByTagName('div')[0];
     downloadText.classList.add('Button');
     downloadText.style.cssText = 'position: absolute; width: 150px; top: -54px; right: 211.26px;';
-    downloadText.querySelector('span > a').innerHTML = 'Download';
     downloadText.querySelector('span').style.cssText = 'font-size: 1rem;';
+    downloadText.querySelector('span > a').innerHTML = 'Download';
 
-    // for delayed script events
-    window.addEventListener('load', function(){
-        // maximize doc preview
-        docPreview.getElementsByTagName('div')[0].style.overflow = 'hidden';
-        docPreview.getElementsByTagName('div')[0].getElementsByTagName('iframe')[0].style.minHeight = '0';
+    // check if element is loaded
+    const checkElement = async selector => {
+        while ( document.querySelector(selector) === null) {
+            await new Promise( resolve => requestAnimationFrame(resolve) )
+        }
+        return document.querySelector(selector);
+    };
+
+    // restyle doc preview window once loaded
+    checkElement('#doc_preview > div').then(el => {
+        el.style.overflow = 'hidden';
+        el.getElementsByTagName('iframe')[0].style.minHeight = '0';
     });
 })();
--- better-grade-view.user.js
+++ better-grade-view.user.js
@@ -2,7 +2,7 @@
 // @name         Better Grade View
 // @namespace    https://alanjyu.com
 // @homepageURL  https://github.com/alanjyu/better-quercus.js
-// @version      0.5
+// @version      0.6.1
 // @description  Display grade as percentages
 // @author       Alan J. Yu
 // @match        http*://q.utoronto.ca/courses/*/grades
@@ -19,12 +19,17 @@
 
     var styleSheet = document.createElement('style')
     styleSheet.innerHTML =
-        '#grades_summary{margin-top: 0;} \
-         #assignments{padding: 0;} \
-        .percentage{text-align: center !important;}';
+        '@media only screen and (min-width: 768px) { \
+             #grades_summary{margin-top: 0;} \
+             #assignments{padding: 0;} \
+             #content{position: relative;} \
+             #print-grades-container{position: absolute; top: 54px; right: 24px;} \
+            .ic-Action-header__Primary{display: none;} \
+            .percentage{text-align: center !important;} \
+         }';
     document.body.appendChild(styleSheet);
 
-    table_head.insertCell(5).outerHTML = '<th scope="col" class="percentage">%</th>'; // insert % column for table head
+    table_head.insertCell(5).outerHTML = '<th scope="col" class="percentage">%</th>'; // insert % column
     table_head.deleteCell(-1) // remove excess column
 
     table_body.forEach(el => {
@@ -32,6 +37,11 @@
         var points = parseFloat(el.querySelector('.what_if_score').innerHTML); // might be a bad implementation
         var points_possible = parseFloat(el.querySelector('.points_possible').innerHTML);
         var perc = Math.round(points/points_possible * 100);
+
+        // change 'NaN' value to '-'
+        if (isNaN(perc)){
+            perc = '-';
+        }
         el.insertCell(5).outerHTML = '<td class="percentage">' + perc + '</td>'; // insert percentage column
     });
 })();
