afed692fe9a4c68dce9a440c7e9282134f8028d9
Alan J. Yu <alanjhyu@gmail.com>
2021-03-09 18:33:23 -0600

    Implement qol features for quiz view

---
+++ better-quiz-view.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Better Quiz View
+// @namespace    https://alanjyu.com
+// @homepageURL  https://github.com/alanjyu/better-quercus.js
+// @version      0.0.1
+// @description  A better quiz taking experience.
+// @run-at       document-idle
+// @author       Alan J. Yu
+// @match        http*://q.utoronto.ca/courses/*/quizzes/*
+// @updateURL    https://raw.githubusercontent.com/alanjyu/better-quercus/main/better-quiz.user.js
+// @downloadURL  https://raw.githubusercontent.com/alanjyu/better-quercus/main/better-quiz.user.js
+// @supportURL   https://github.com/alanjyu/better-quercus.js/issues
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  var styleSheet = document.createElement('style')
+  styleSheet.innerHTML =
+      '#right-side{top: 56px; position: sticky;} \
+       #question_list{max-height: 500px !important;} \
+       .list_question.marked > .jump_to_question_link{color: red !important;} \
+       .question.marked > .header{background: red; color: white;}';
+  document.body.appendChild(styleSheet);
+})();
